ed476785bca9b97e684f528813421607ac859e69
Chris Baldelomar <chris@webplant.media>
2026-01-15 11:39:44 -0600

    Remove old eslint-plugin and cli packages (now in canon)

--- eslint-plugin/dist/rules/no-container-in-section.d.ts
+++
@@ -1,5 +0,0 @@
-import { ESLintUtils } from '@typescript-eslint/utils';
-declare const _default: ESLintUtils.RuleModule<"noContainerInSection", [], unknown, ESLintUtils.RuleListener> & {
-    name: string;
-};
-export default _default;
--- eslint-plugin/dist/rules/prefer-component-props.d.ts
+++
@@ -1,5 +0,0 @@
-import { ESLintUtils } from '@typescript-eslint/utils';
-declare const _default: ESLintUtils.RuleModule<"preferComponentProps", [], unknown, ESLintUtils.RuleListener> & {
-    name: string;
-};
-export default _default;
